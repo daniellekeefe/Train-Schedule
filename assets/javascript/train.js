21e2b92e7e55f1d2855d74b83d9dac9ba0658636
olive <daniellek@mobilefuse.com>
2019-10-28 20:40:34 -0400

    added the function for the table to be manipulated by Jquery

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -4,16 +4,16 @@ $(document).ready(function(){
 // Initialize Firebase
 // Make sure to match the configuration to the script version number in the HTML
 // (Ex. 3.0 != 3.7.0)
-let config = {
-        apiKey: "AIzaSyBIoYRYn2gkYW7YGVSgPvS2jDiIakWMuCo",
-        authDomain: "traintimes-68e6e.firebaseapp.com",
-        databaseURL: "https://traintimes-68e6e.firebaseio.com",
-        projectId: "traintimes-68e6e",
-        storageBucket: "traintimes-68e6e.appspot.com",
-        messagingSenderId: "15461218892",
-        appId: "1:15461218892:web:7652b07bef93e8795cd2e9",
-        measurementId: "G-EMS958C48S"
-      };
+var config = {
+    apiKey: "AIzaSyAXgG7_7r77PfCQ4nrdcnKq-Ev0_GhIQNo",
+    authDomain: "danielle-test-project.firebaseapp.com",
+    databaseURL: "https://danielle-test-project.firebaseio.com",
+    projectId: "danielle-test-project",
+    storageBucket: "danielle-test-project.appspot.com",
+    messagingSenderId: "375960205835",
+    appId: "1:375960205835:web:2eb57cccde64468a04650e",
+    measurementId: "G-KHQDC7VXLW"
+  };
   
   firebase.initializeApp(config);
   
@@ -21,13 +21,20 @@ let config = {
   // Create a variable to reference the database.
   let database = firebase.database();
 
-$('add-train-btn').on('click',function(event){
+$('#add-train-btn').on('click',function(event){
 event.preventDefault();
 //take user input
-Const trainName: $('#train-name-input').val().trim();
-Const destination: $('#destination-input').val().trim();
-Let trainTime: $('#first-train-input').val().trim();
-Let trainFreq: $('#frequency-input').val().trim();
+const trainName = $('#train-name-input').val().trim();
+const destination  = $('#destination-input').val().trim();
+let trainTime = $('#first-train-input').val().trim();
+let trainFreq = $('#frequency-input').val().trim();
+var markup = "<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + trainFreq + "</td></tr>";
+            $("table tbody").append(markup);
+//consolelog everything!
+console.log(trainName);
+console.log(destination);
+console.log(trainTime);
+console.log(trainFreq);
 
 //create temp obj for train data
 let newTrain={
@@ -38,18 +45,23 @@ let newTrain={
 };
 database.ref().push(newTrain);
 
-//consolelog everything!
-console.log(trainName.name);
-console.log(destination.dest);
-console.log(trainTime.time);
-console.log(trainFreq.freq);
+
 
 alert("train successfully ADDED");
-
+//adds content into the schedule
 $('#train-name-input').val('');
 $('#rdestination-input').val('');
 $('#first-train-input').val(''); 
 $('#frequency-input').val('');
+
+function myCreateFunction() {
+    var table = $("#train-table");
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = "NEW CELL1";
+    cell2.innerHTML = "NEW CELL2";
+  }
 
 })
 
